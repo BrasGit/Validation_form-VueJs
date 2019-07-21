09a0b8e7b62fd30023b4f0fc2e04124ac626a4ca
BrasGit <romabraslavski@gmail.com>
2019-07-21 11:20:31 +0300

    routing

---
+++ src/pages/CarFull.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h4>Car name: Ford</h4>
+        <h5>Car year: 2019</h5>
+
+        <div class="scroll"></div>
+
+        <h6 id="scroll" >Hello from the bottom</h6>
+    </div>
+</template>
+
+<script>
+export default {
+    beforeRouteEnter(to, from, next){
+                        console.log('beforeRouteEnter')
+                        next(true)
+                    }
+}
+</script>
+
+<style scoped>
+.scroll{
+    width:100px;
+    height: 1000px;
+    background: #ccc;
+}
+</style>
---
+++ src/pages/Error.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+      <h1>404 Error</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    
+    
+}
+</script>
